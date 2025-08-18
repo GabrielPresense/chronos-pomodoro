f923cc18dbdfc354bb14a56003d041c466edac68
Gabriel Presense <gabrielpresense@gmail.com>
2025-08-18 16:54:45 -0300

    17. Finalização do componente Heading e tipagem TypeScript de children (ReactNode)

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,16 @@ import { Heading } from './components/Heading';
 
 import './styles/theme.css'
 import './styles/global.css'
+import { TimerIcon } from 'lucide-react';
 
 export function App() {
     return (
         <>
-            <Heading attr={123} attr2="String">
+            <Heading>
                 Olá mundo 1
+                <button>
+                    <TimerIcon />
+                </button>
             </Heading>
             <p>
                 Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.
