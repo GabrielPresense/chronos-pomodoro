55726fe13cb0e15fdb59129b03f3ee247f996aa9
Gabriel Presense <gabrielpresense@gmail.com>
2025-08-18 17:50:05 -0300

    19. Criando a estrutura HTML e CSS do nosso app para cada seção do site

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,31 @@
-import { Heading } from './components/Heading';
 
 import './styles/theme.css'
 import './styles/global.css'
-import { TimerIcon } from 'lucide-react';
 
 export function App() {
-    return (
-        <>
-            <Heading>
-                Olá mundo 1
-                <button>
-                    <TimerIcon />
-                </button>
-            </Heading>
-            <p>
-                Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.
-            </p>
-        </>
-    );
+    return <>
+        <div className="container">
+            <div className="content">
+                <section>LOGO</section>
+            </div>
+        </div>
+
+        <div className="container">
+            <div className="content">
+                <section>MENU</section>
+            </div>
+        </div>
+
+        <div className="container">
+            <div className="content">
+                <section>FORM</section>
+            </div>
+        </div>
+
+        <div className="container">
+            <div className="content">
+                <section>FOOTER</section>
+            </div>
+        </div>
+    </>;
 }
