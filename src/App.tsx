68c2c96256127bbe33b8eced9bc10af920e88e48
Gabriel Presense <gabrielpresense@gmail.com>
2025-08-20 10:34:29 -0300

    Input: adicionando estilos de CSS para input ativo, em foco e desativado

--- src/App.tsx
+++ src/App.tsx
@@ -29,6 +29,7 @@ export function App() {
                     labelText='task'
                     id='meuInput'
                     type='text'
+                    placeholder='Digite algo'
                     />
                 </div>
 
--- src/components/DefaultInput/index.tsx
+++ src/components/DefaultInput/index.tsx
@@ -1,13 +1,20 @@
+import styles from './styles.module.css'
+
 type DefaultInputProps = {
     id: string;
     labelText: string;
 } & React.ComponentProps<'input'>;
 
-export function DefaultInput({ id, type, labelText, ...rest }: DefaultInputProps) {
+export function DefaultInput({
+    id,
+    type,
+    labelText,
+    ...rest
+}: DefaultInputProps) {
     return (
         <>
             {labelText && <label htmlFor={id}>{labelText}</label>}
-            <input id={id} type={type} {...rest} />
+            <input className={styles.input} id={id} type={type} {...rest} />
         </>
     );
 }
